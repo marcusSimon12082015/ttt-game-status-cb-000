a40f5cee64e1b3df46a3a70c38c323975ac2734f
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-08 09:54:16 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,24 +16,27 @@ WIN_COMBINATIONS = [
 ]
 def won?(board)
   #check if the board is empty
-  return board.any?{|i| i != " "}
+  if board.any?{|i| i != " "}
+    #board is empty
+    return false
+  else
+    #check the wining combinations
+    WIN_COMBINATIONS.each do |combination|
+      #we get the indexes of a winning combination
+      win_index1 = combination[0]
+      win_index2 = combination[1]
+      win_index3 = combination[2]
+      #we get the content of those winning combination indexses
+      position1 = board[win_index1]
+      position2 = board[win_index2]
+      position3 = board[win_index3]
 
-  #check the wining combinations
-  WIN_COMBINATIONS.each do |combination|
-    #we get the indexes of a winning combination
-    win_index1 = combination[0]
-    win_index2 = combination[1]
-    win_index3 = combination[2]
-    #we get the content of those winning combination indexses
-    position1 = board[win_index1]
-    position2 = board[win_index2]
-    position3 = board[win_index3]
-
-    if (position1 == "X" && position2 == "X" && position3 == "X") ||
-       (position1 == "O" && position2 == "O" && position3 == "O")
-      return combination
-    else
-      return false
+      if (position1 == "X" && position2 == "X" && position3 == "X") ||
+         (position1 == "O" && position2 == "O" && position3 == "O")
+        return combination
+      else
+        return false
+      end
     end
   end
 end
