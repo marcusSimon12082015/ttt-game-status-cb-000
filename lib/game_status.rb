6a22fba473e8fa3b295f26f0fe2202df0c9d3b3a
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-08 09:24:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 ]
 def won?(board)
   #check if the board is empty
-  board.any?{|i| i != " "}? false : true
+  return board.any?{|i| i != " "}? false : true
 
   #check the wining combinations
   WIN_COMBINATIONS.each do |combination|
